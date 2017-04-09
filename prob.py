1d8b154c9358db24fa97d4b0cfc5ae3176c23106
Mukul <mukulkhandelwal1997@gmail.com>
2017-04-09 16:52:27 +0530

    p4

--- prob.py
+++ prob.py
@@ -11,6 +11,9 @@ from tflearn.layers.estimator import regression
 
 import tensorflow as tf
 
+
+import matplotlib.pyplot as plt
+
 TRAIN_DIR = ''
 TEST_DIR = ''
 
@@ -133,5 +136,54 @@ model.save(MODEL_NAME)
 
 
 
+
+#visually inspection
+
+
+
+# if you need to create the data:
+#test_data = process_test_data()
+# if you already have some saved:
+test_data = np.load('test_data.npy')
+
+fig=plt.figure()
+
+for num,data in enumerate(test_data[:12]):
+    # cat: [1,0]
+    # dog: [0,1]
+    
+    img_num = data[1]
+    img_data = data[0]
+    
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    #model_out = model.predict([data])[0]
+    model_out = model.predict([data])[0]
+    
+    if np.argmax(model_out) == 1: str_label='Dog'
+    else: str_label='Cat'
+        
+    y.imshow(orig,cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
+
+
+
+
+
+with open('submission_file.csv','w') as f:
+    f.write('id,label\n')
+            
+with open('submission_file.csv','a') as f:
+    for data in tqdm(test_data):
+        img_num = data[1]
+        img_data = data[0]
+        orig = img_data
+        data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+        model_out = model.predict([data])[0]
+        f.write('{},{}\n'.format(img_num,model_out[1]))
 
 
