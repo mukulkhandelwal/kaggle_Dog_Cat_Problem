05c107de2e7f09734e559a27a39430c4f424d8f7
Mukul <mukulkhandelwal1997@gmail.com>
2017-04-09 12:32:41 +0530

    Building the network

--- prob.py
+++ prob.py
@@ -4,6 +4,11 @@ import os
 from random import shuffle
 from tqdm import tqdm
 
+import tflearn
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
 
 TRAIN_DIR = ''
 TEST_DIR = ''
@@ -41,5 +46,41 @@ def create_train_data():
 	return training_data
 
 
+def process_test_data():
+	testing_data = []
+	for img in tqdm(os.listdir(TEST_DIR)):
+		path = os.path.join(TEST_DIR,img)
+		img_num = img.split('.')[0]
+		img = cv2.resize(cv2.imread(path,cv2.IMREAD_GRAYSCALE),(IMG_SIZE,IMG_SIZE))
+		testing_data.append([np.array(img),img_num])
+
+
+	np.save('test_data.npy',testing_data)
+	return testing_data
+
+
+train_data = create_train_data()
+
+#if you already have train data:
+#train_data = np.load('train_data.npy')
+
+
+#convnet 
+
+convnet = input_data(shape=[None,IMG_SIZE, IMG_SIZE, 1], name='input')
+
+convnet = conv_2d(convnet, 32, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = max_pool_2d(convnet, 2)
+
+convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = dropout(convnet, 0.8)
+
+convnet = fully_connected(convnet, 2, activation='softmax')
+convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+
+model = tflearn.DNN(convnet, tensorboard_dir = 'log')
 
 
